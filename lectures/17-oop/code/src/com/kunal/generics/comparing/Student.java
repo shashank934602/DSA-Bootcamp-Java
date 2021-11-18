a6adcac8aedc1e818d75da0cd54c3ff837b04110
shashank934602 <93077450+shashank934602@users.noreply.github.com>
2021-11-18 10:27:34 +0530

    Update Student.java

--- lectures/17-oop/code/src/com/kunal/generics/comparing/Student.java
+++ lectures/17-oop/code/src/com/kunal/generics/comparing/Student.java
@@ -1,18 +1,21 @@
 package com.kunal.generics.comparing;
 
 public class Student implements Comparable<Student>{
-    int rollno;
+    int rollno; // variables of roll no and marks
     float marks;
-
+    
+// having a constructor of rollno and marks
     public Student(int rollno, float marks) {
         this.rollno = rollno;
         this.marks = marks;
     }
+    // overided string method
 
     @Override
     public String toString() {
         return marks + "";
     }
+     // created a methon in the name of campareTo and used student class and we also used object so that we can have our own genirics 
 
     @Override
     public int compareTo(Student o) {
